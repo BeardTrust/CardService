1cfaa77fec40d708943cdeef834f0cf1638b673c
DavisHill-SS <davis.hill@smoothstack.com>
2021-08-05 10:29:36 -0500

    updated entity/dto

--- src/main/java/com/beardtrust/webapp/cardservice/dtos/CardDTO.java
+++ src/main/java/com/beardtrust/webapp/cardservice/dtos/CardDTO.java
@@ -2,16 +2,24 @@ package com.beardtrust.webapp.cardservice.dtos;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+
 import lombok.Data;
 
 @Data
 public class CardDTO implements Serializable{
 	
 	private String cardId;
-	private String cardNumber;
-	private String expiration;
+	private String userId;
 	private String cardType;
-	private String account_id;
-	private String user_id;
+	private Float balance;
+	private String cardNumber;
+	private Float interestRate;
+	private String createDate;
+	private String nickname;
+	private Integer billCycleLength;
+	private Boolean activeStatus;
+	private String expireDate;
 
 }
--- src/main/java/com/beardtrust/webapp/cardservice/entities/CardEntity.java
+++ src/main/java/com/beardtrust/webapp/cardservice/entities/CardEntity.java
@@ -16,55 +16,21 @@ public class CardEntity implements Serializable{
 	@Id
 	@Column(unique=true)
 	private String cardId;
+	@OneToMany
+	private String userId;
+	private String cardType;
+	private Float balance;
 	@Column(unique=true)
 	private String cardNumber;
-	private String expiration;
-	private String cardType;
+	private Float interestRate;
+	private String createDate;
+	private String nickname;
+	private Integer billCycleLength;
+	private Boolean activeStatus;
+	private String expireDate;
+
 	@OneToOne
 	private String account_id;
-	@OneToMany
-	private String user_id;
-	
-	public String getCardId() {
-		return cardId;
-	}
-	public void setCardId(String cardId) {
-		this.cardId = cardId;
-	}
-	public String getCardNumber() {
-		return cardNumber;
-	}
-	public void setCardNumber(String cardNumber) {
-		this.cardNumber = cardNumber;
-	}
-	public String getExpiration() {
-		return expiration;
-	}
-	public void setExpiration(String expiration) {
-		this.expiration = expiration;
-	}
-	public String getCardType() {
-		return cardType;
-	}
-	public void setCardType(String cardType) {
-		this.cardType = cardType;
-	}
-	public String getAccount_id() {
-		return account_id;
-	}
-	public void setAccount_id(String account_id) {
-		this.account_id = account_id;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	@Override
-	public String toString() {
-		return "CardEntity [cardId=" + cardId + ", cardNumber=" + cardNumber + ", expiration=" + expiration
-				+ ", cardType=" + cardType + ", account_id=" + account_id + ", user_id=" + user_id + "]";
-	}
+
 	
 }
