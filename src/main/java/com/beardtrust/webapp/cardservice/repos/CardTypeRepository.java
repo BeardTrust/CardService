2e59a7da3f79fd1de33fbe2e50832eacd4154c7c
Matthew Crowell <>
2021-08-16 15:08:54 -0500

    add method to retrieve all cards by availability to card type rep

--- src/main/java/com/beardtrust/webapp/cardservice/repos/CardTypeRepository.java
+++ src/main/java/com/beardtrust/webapp/cardservice/repos/CardTypeRepository.java
@@ -4,6 +4,8 @@ import com.beardtrust.webapp.cardservice.entities.CardTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * This class gives access to the card types found in the database.
  *
@@ -11,4 +13,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CardTypeRepository extends JpaRepository<CardTypeEntity, String> {
+	List<CardTypeEntity> getAllByIsAvailable(Boolean availability);
 }
